68f6661b2ae3223ee4779d5a475621fef73c83fc
explodi <phaser@tubatuba.net>
2017-12-27 16:32:57 -0300

    rm _site

---
+++ laucha.js
@@ -0,0 +1,10 @@
+$.getJSON('https://mindicador.cl/api', function(data) {
+    console.log(data.dolar.valor)
+}).fail(function() {
+    console.log('Error al consumir la API!');
+});
+$.getJSON('https://api.coinmarketcap.com/v1/ticker/?limit=100', function(data) {
+    console.log(data[1].price_usd)
+}).fail(function() {
+    console.log('Error al consumir la API!');
+});
