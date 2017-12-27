789ff95a53f406bce6df99f476ed238adb466b0b
explodi <phaser@tubatuba.net>
2017-12-27 16:41:27 -0300

    precio en pesos

--- laucha.js
+++ laucha.js
@@ -1,10 +1,14 @@
+var usd;
+var eth;
 $.getJSON('https://mindicador.cl/api', function(data) {
     console.log(data.dolar.valor)
+    usd=data.dolar.valor;
 }).fail(function() {
     console.log('Error al consumir la API!');
 });
 $.getJSON('https://api.coinmarketcap.com/v1/ticker/?limit=100', function(data) {
     console.log(data[1].price_usd)
+    eth=data[1].price_usd;
 }).fail(function() {
     console.log('Error al consumir la API!');
 });
